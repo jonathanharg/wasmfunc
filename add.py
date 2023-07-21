6f23384976b20cfbac03050506fb4ba4f14c273e
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2023-07-21 15:32:34 +0100

    Few settings and changes

---
+++ add.py
@@ -0,0 +1,10 @@
+import pygwasm
+
+@pygwasm.func
+def add(x: pygwasm.i32, y: pygwasm.i32):
+    return x + y
+
+def standard(x):
+    ajsldfj = 5
+    print("AJLSDJFLKSJF")
+    ajsldfj += 1
--- pygwasm.py
+++ pygwasm.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python
+from _ast import FunctionDef, Module
+import ast
+from typing import Any, TypeAlias
+import symtable
 
+i32: TypeAlias = int
+
+class Visitor(ast.NodeVisitor):
+    def visit_Module(self, node: Module):
+        print(f"Creating WASM Module")
+        return super().generic_visit(node)
+    
+    def visit_FunctionDef(self, node: FunctionDef):
+        if not any([dec.value.id == 'pygwasm' for dec in node.decorator_list]):
+            print(f"Ignoring non WASM function {node.name}")
+            return
+        print(f"Creating WASM Function {node.name}")
+        return super().generic_visit(node)
+    
+    def generic_visit(self, node):
+        print(f"Node of type {node.__class__.__name__} is not supported by pygwams. Line number {node.lineno if hasattr(node, 'lineno') else '?'}")
+        return super().generic_visit(node)
 
 if __name__ == "__main__":
     import sys
-    import ast
 
     # On MacOS
     # brew intall binaryen
@@ -15,28 +35,10 @@ if __name__ == "__main__":
     for path in sys.argv[1:]:
         with open(path, "r") as file:
             print(f"Compiling {path}...")
-            tree = ast.parse(file.read())
-            # TODO: Module parsing
-            functions: list[ast.FunctionDef] = []
-            for node in tree.body:
-                if isinstance(node, ast.FunctionDef):
-                    for d in node.decorator_list:
-                        # FIXME: This wont work with import aliases
-                        if (
-                            isinstance(d, ast.Attribute)
-                            & isinstance(d.value, ast.Name)
-                            & (d.value.id == "pygwasm")
-                            & (d.attr == "func")
-                        ):
-                            functions.append(node)
-            print("========= WHOLE FILE ========")
-            # print(ast.dump(tree, indent=2))
-            # module = binaryen.SetColorsEnabled(False)
-            for func in functions:
-                print(f"===== WASM Function \"{func.name}\" ======")
-
-
-                print(ast.dump(func, indent=2))
+            tree = ast.parse(file.read(), filename=path, type_comments=True)
+            print(ast.dump(tree, indent=2))
+            visitor = Visitor()
+            visitor.visit(tree)
 
 else:
 
