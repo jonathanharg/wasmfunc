e9ee84826a085e24c96e111fe163118e7c1c6d8c
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2024-01-19 12:35:24 +0000

    Improved file handling

--- pygwasm/__main__.py
+++ pygwasm/__main__.py
@@ -1,19 +1,9 @@
 import sys
-from .compiler import Compiler
-import ast
-import symtable
-import os
+from .file_handler import compile_file, generate_output_name
 
 for string_path in sys.argv[1:]:
-    with open(string_path, "r", encoding="utf-8") as file:
-        print(f"Compiling {string_path}...")
-        code = file.read()
-        path =  os.path.split(string_path)
-        filename = path[-1]
-        tree = ast.parse(code, filename=filename, type_comments=True)
-        symbol_table = symtable.symtable(code, filename, "exec")
-        compiler = Compiler(symbol_table)
-        compiler.compile(tree)
-        compiler.module.optimize()
-        # visitor.module.write_binary(filename)
-        compiler.module.print()
+    print(f"Compiling {string_path}...")
+    compiler = compile_file(string_path)
+    compiler.module.optimize()
+    filename = generate_output_name(string_path)
+    compiler.module.write_binary(filename)
---
+++ pygwasm/file_handler.py
@@ -0,0 +1,41 @@
+import ast
+import os
+import symtable
+from .compiler import Compiler
+
+def compile_file(input_path: str):
+    with open(input_path, "r", encoding="utf-8") as file:
+        code = file.read()
+        path =  os.path.split(input_path)
+        filename = path[-1]
+        tree = ast.parse(code, filename=filename, type_comments=True)
+        symbol_table = symtable.symtable(code, filename, "exec")
+        compiler = Compiler(symbol_table)
+        compiler.compile(tree)
+        return compiler
+
+def generate_output_path(input_path: str, binary = True):
+    """Generates a corresponding output path for an input path. E.g. `a/b/c.py` becomes `a/b/c.wasm`.
+
+    Args:
+        input_path (str): Input file path
+        binary (bool, optional): If the output is in binary format. Binary format has the extension `.wasm`, otherwise use the text format `.wat`. Defaults to True.
+    """
+    # path =  os.path.split(input_path)
+    # filename_and_extension = path[-1].split()
+    filename, _file_extension = os.path.splitext(input_path)
+    
+    if binary:
+        return filename + ".wasm"
+    
+    return filename + ".wat"
+
+def generate_output_name(input_path: str, binary = True):
+    """Generates a corresponding output name for an input path. E.g. `a/b/c.py` becomes `c.wasm`.
+
+    Args:
+        input_path (str): Input file path
+        binary (bool, optional): If the output is in binary format. Binary format has the extension `.wasm`, otherwise use the text format `.wat`. Defaults to True.
+    """
+    path = generate_output_path(input_path, binary)
+    return os.path.split(path)[-1]
